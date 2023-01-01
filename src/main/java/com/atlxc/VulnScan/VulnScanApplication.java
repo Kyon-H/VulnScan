33dbca5c943f63c72458cfa8269c52573a681513
Kyon-H <a3171218907@qq.com>
2023-01-01 16:24:10 +0800

    初始化

---
+++ src/main/java/com/atlxc/VulnScan/VulnScanApplication.java
@@ -0,0 +1,13 @@
+package com.atlxc.VulnScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnScanApplication.class, args);
+	}
+
+}

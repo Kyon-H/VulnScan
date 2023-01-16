600cddc7a2881fc31930eeb19526e5c28d318437
Kyon-H <a3171218907@qq.com>
2023-01-16 22:20:26 +0800

    awvs api

--- src/main/java/com/atlxc/VulnScan/config/ConfigConstant.java
+++ src/main/java/com/atlxc/VulnScan/config/ConfigConstant.java
@@ -6,7 +6,7 @@
  * 版权所有，侵权必究！
  */
 
-package com.atlxc.VulnScan.utils;
+package com.atlxc.VulnScan.config;
 
 /**
  * 系统参数相关Key
@@ -18,5 +18,7 @@ public class ConfigConstant {
      * 云存储配置KEY
      */
     public final static String CLOUD_STORAGE_CONFIG_KEY = "CLOUD_STORAGE_CONFIG_KEY";
+
+    public static final String AWVS_API_URL="https://desktop-jv0cb08:3443/api/v1/";
     public final static String AWVS_API_KEY="1986ad8c0a5b3df4d7028d5f3c06e936cd85c6c2e5fac4f398dde2b38c66881d4";
 }

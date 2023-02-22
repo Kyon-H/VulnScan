a7723176eb97bf3fe35502414294726f7b3a9ca2
Kyon-H <a3171218907@qq.com>
2023-02-22 22:37:46 +0800

    获取漏洞列表

--- src/main/java/com/atlxc/VulnScan/product/service/VulnInfoService.java
+++ src/main/java/com/atlxc/VulnScan/product/service/VulnInfoService.java
@@ -16,5 +16,7 @@ import java.util.Map;
 public interface VulnInfoService extends IService<VulnInfoEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    Boolean updateAll();
 }
 
--- src/main/java/com/atlxc/VulnScan/product/service/impl/VulnInfoServiceImpl.java
+++ src/main/java/com/atlxc/VulnScan/product/service/impl/VulnInfoServiceImpl.java
@@ -1,6 +1,19 @@
 package com.atlxc.VulnScan.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.atlxc.VulnScan.product.apiservice.VulnService;
+import com.atlxc.VulnScan.product.entity.ScanRecordEntity;
+import com.atlxc.VulnScan.product.service.ScanRecordService;
+import com.atlxc.VulnScan.product.service.UsersService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -12,18 +25,67 @@ import com.atlxc.VulnScan.product.dao.VulnInfoDao;
 import com.atlxc.VulnScan.product.entity.VulnInfoEntity;
 import com.atlxc.VulnScan.product.service.VulnInfoService;
 
-
+@Slf4j
 @Service("vulnInfoService")
 public class VulnInfoServiceImpl extends ServiceImpl<VulnInfoDao, VulnInfoEntity> implements VulnInfoService {
+    @Autowired
+    VulnService vulnService;
+    @Autowired
+    UsersService usersService;
+    @Autowired
+    ScanRecordService scanRecordService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String userName=params.get("userName").toString();
+        List<ScanRecordEntity> scanRecord=scanRecordService.getByUserName(userName);
+        List<Integer>scanRecordIds=new ArrayList<>();
+        scanRecord.forEach(scanRecordEntity -> {
+            Integer scanRecordId=scanRecordEntity.getId();
+            scanRecordIds.add(scanRecordId);
+        });
         IPage<VulnInfoEntity> page = this.page(
                 new Query<VulnInfoEntity>().getPage(params),
-                new QueryWrapper<VulnInfoEntity>()
+                new QueryWrapper<VulnInfoEntity>().in("scan_record_id",scanRecordIds)
         );
-
         return new PageUtils(page);
+    }
+
+    @Override
+    public Boolean updateAll() {
+        log.info("updateAll");
+        Integer counts = vulnService.getAllVulns(1, "open").getJSONObject("pagination").getInteger("count");
+        while (counts>0){
+            Integer l=counts>=20?20:counts;
+            JSONArray jsonArray = vulnService.getAllVulns(l, "open").getJSONArray("vulnerabilities");
+            for(int i=0;i<jsonArray.size();i++){
+                JSONObject item = jsonArray.getJSONObject(i);
+                String targetId=item.getString("target_id");
+                ScanRecordEntity scanRecord=scanRecordService.getByTargetId(targetId);
+                if(scanRecord!=null){
+                    VulnInfoEntity vulnInfo;
+                    vulnInfo=baseMapper.selectOne(new QueryWrapper<VulnInfoEntity>().eq("vuln_id",item.getString("vuln_id")));
+                    if(vulnInfo==null){
+                        vulnInfo=new VulnInfoEntity();
+                        vulnInfo.setScanRecordId(scanRecord.getId());
+                        vulnInfo.setVulnId(item.getString("vuln_id"));
+                        vulnInfo.setDescription(item.getString("description"));
+                        vulnInfo.setSeverity(item.getInteger("severity"));
+                        vulnInfo.setVulnerability(item.getString("vt_name"));
+                        vulnInfo.setTargetAddress(item.getString("affects_url"));
+                        baseMapper.insert(vulnInfo);
+                    }
+//                    else {
+//                        vulnInfo.setScanRecordId(scanRecord.getId());
+//                        vulnInfo.setVulnId(item.getString("vuln_id"));
+//                        vulnInfo.setDescription(item.getString("description"));
+//                        baseMapper.updateById(vulnInfo);
+//                    }
+                }
+            }
+            counts-=20;
+            }
+        return Boolean.TRUE;
     }
 
 }

3728c9eda482a63fcd032e59710534e8b24e1cd1
Kyon-H <a3171218907@qq.com>
2023-02-24 16:42:23 +0800

    提取loadPage

--- src/main/resources/static/js/vulnerabilities.js
+++ src/main/resources/static/js/vulnerabilities.js
@@ -2,68 +2,29 @@ var currPage=1;
 var pageSize=3;
 var totalCount
 var totalPage
+var URL="/vulninfo/list";
 ////////////////////////////////
 function load(){
-    loadPage(currPage,pageSize);
-}
-//分页查询
-function loadPage(page,limit){
-    let postdata={
-        page:page,
-        limit:limit,
-        sidx:"last_seen",
-        order:"desc"
-    };
-    $.post('/vulninfo/list',
-        postdata,
-        function(data){
-            console.log(data.page);
-            if(data.code==0){
-                // 更新数据列表
-                addVulnInfo(data.page);
-                // 更新分页组件
-                var $pagination = $('.pagination');
-                var $pages = $pagination.find('.page-item:not(#pagePre,#pageNext)');
-                $pages.removeClass('active');
-                //更新
-                if(page==1){
-                    $('#pagePre').parent().addClass('disabled');
-                    $('#page1').parent().addClass('active');
-                }else if(page==totalPage){
-                    $('#pageNext').parent().addClass('disabled');
-                    $('#page3').parent().addClass('active');
-                }else{
-                    $('#page1').text(page-1);
-                    $('#page2').text(page);
-                    $('#page3').text(page+1);
-                    $('#page2').parent().addClass('active');
-                    $('#pagePre').parent().removeClass('disabled');
-                    $('#pageNext').parent().removeClass('disabled');
-                }
-            }else{
-                layer.msg(data.msg, {icon:2});
-            }
-        }
-    );
+    loadPage(URL,currPage,pageSize,"last_seen","desc",addVulnInfo);
 }
 //绑定上一页按钮点击事件
 $('#pagePre').click(function(){
     let currentPage = parseInt($('.page-item.active a').text());
     if (currentPage > 1) {
-      loadPage(currentPage - 1,pageSize);
+      loadPage(URl,currentPage - 1,pageSize,"last_seen","desc",addVulnInfo);
     }
 });
 // 绑定下一页按钮点击事件
 $('#pageNext').click(function(){
    let currentPage = parseInt($('.page-item.active a').text());
    if (currentPage < totalPage) {
-     loadPage(currentPage + 1,pageSize);
+     loadPage(URL,currentPage + 1,pageSize,"last_seen","desc",addVulnInfo);
    }
 })
 // 绑定页码按钮点击事件
 $('.page-link:not(#pagePre,#pageNext)').click(function(e) {
     let page = parseInt($(this).text());
-    loadPage(page,pageSize);
+    loadPage(URL,page,pageSize,"last_seen","desc",addVulnInfo);
 });
 //
 function addVulnInfo(data){
